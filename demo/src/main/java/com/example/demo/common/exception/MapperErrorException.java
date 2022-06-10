fa1b3703bce8a2af8b2d4caa998ca142c2fa8e6c
wjdgustn <wjdgustn15@naver.com>
2022-06-10 15:25:19 +0900

    refactor : remove Mapper Pojo & Exception
    * because of unused class

--- demo/src/main/java/com/example/demo/common/exception/MapperErrorException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.common.exception;
-
-public class MapperErrorException extends BaseException {
-  private static final  String MSG = "mapper 문제입니다.서비측에 문의해주세요.";
-  public MapperErrorException() {
-    super(MSG);
-  }
-}
-
