4dd41d3fdbcdaa6c6ae8a2a478a062ed24e743cd
wjdgustn <wjdgustn15@naver.com>
2022-06-10 15:17:32 +0900

    refactor: remove Mapper
    * Mapper가 objectMapper와 기능이 같아서, 삭제함.

--- demo/src/main/java/com/example/demo/user/service/UserService.java
+++ demo/src/main/java/com/example/demo/user/service/UserService.java
@@ -1,12 +1,12 @@
 package com.example.demo.user.service;
 
-import com.example.demo.common.util.Mapper;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.dto.LoginRequest;
 import com.example.demo.user.dto.LoginResponse;
 import com.example.demo.user.exception.NotFoundUserException;
 import com.example.demo.user.repository.UserQueryRepository;
 import com.example.demo.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,10 +16,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
-  private final Mapper mapper;
   private final UserQueryRepository userQueryRepository;
+  private final ObjectMapper objectMapper;
+
   public LoginResponse login(LoginRequest loginRequest){
-    return mapper.mapper(userQueryRepository.existUser(loginRequest).orElseThrow(NotFoundUserException::new),LoginResponse.class);
+    return objectMapper.convertValue(userQueryRepository.existUser(loginRequest).orElseThrow(NotFoundUserException::new),LoginResponse.class);
   }
   public User findById(long id){
     return userRepository.findById(id).orElseThrow(NotFoundUserException::new);
